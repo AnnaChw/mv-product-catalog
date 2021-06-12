cd22280dbb12a35b7b04c859d40608bdea7298ba
Anna Ch <ania.kal@gmail.com>
2021-06-12 15:38:57 +0200

    Project initial skeleton

---
+++ src/main/java/com/mcsystems/mvproductcatalog/MvProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.mcsystems.mvproductcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvProductCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcsystems/mvproductcatalog/MvProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcsystems.mvproductcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
